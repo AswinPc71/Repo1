34c77fc87e852e41eded9ecfcb61c4d7fcddbe20
Aswin P Chacko <Aswin P Chacko@IDEAPAD-G3.lan>
2026-09-14 22:44:10 +0530

    initial commit

---
+++ maven_project1/src/main/java/pack1/Abc.java
@@ -0,0 +1,13 @@
+package pack1;
+
+public class Abc {
+
+	public static void main(String[] args) {
+		int num1 = 89;
+		int num2 =74;
+		int sum = num1 + num2;
+		
+		System.out.println(num1 + " + " + num2 + " = " + sum);
+	}
+
+}
